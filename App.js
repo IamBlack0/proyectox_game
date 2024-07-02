db41bab523c31ef4d74ddd9a9737e36f284b0fc0
black <blacknegocio3@gmail.com>
2024-07-01 19:58:31 -0500

    Acutalizacion 1.1 prematura

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HOLA MUNDO</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    const [isLoading, setLoading] = useState(true);
+    const [usuarios, setUsuarios] = useState([]);
+
+    const getUsuarios = async () => {
+        try {
+            const response = await fetch('http://192.168.0.3:8080/usuarios/all'); // Usa la IP de tu máquina
+            const json = await response.json();
+            console.log('Datos obtenidos:', json); // Verificar los datos obtenidos
+            // Filtrar datos incompletos
+            const validData = json.filter(item => item && item.id_usuario !== undefined);
+            setUsuarios(validData);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getUsuarios();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    data={usuarios}
+                    keyExtractor={(item, index) => index.toString()} // Usar índice como clave temporalmente
+                    renderItem={({ item }) => (
+                        <View style={styles.item}>
+                            <Text>{item.nombre_usuario}</Text>
+                            <Text>{item.correo_usuario}</Text>
+                        </View>
+                    )}
+                />
+            )}
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
 });
+
+export default App;
